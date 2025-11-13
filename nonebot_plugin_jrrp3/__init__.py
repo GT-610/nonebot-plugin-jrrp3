0496b34aef2b4fc0584453797f6297b7e4acb397
GT610 <myddz1005@163.com>
2025-11-13 21:43:53 +0800

    feat: 自定义运势配置
    - 更新版本号至3.2.0
    - 修改插件名称为"每日人品 3"
    - 添加对自定义运势范围的支持，通过YAML配置文件实现
    - 更新README文档，详细说明自定义运势配置方法
    - 添加PyYAML作为新依赖项

--- nonebot_plugin_jrrp3/__init__.py
+++ nonebot_plugin_jrrp3/__init__.py
@@ -3,7 +3,7 @@ version: 3.0
 Author: Rene8028
 Date: 2022-07-20 21:58:25
 LastEditors: GT-610
-LastEditTime: 2025-11-11 17:05:19
+LastEditTime: 2025-11-13 21:41:55
 '''
 
 import datetime
@@ -110,8 +110,8 @@ plugin_config = None
 from nonebot.plugin import PluginMetadata, inherit_supported_adapters
 
 __plugin_meta__ = PluginMetadata(
-    name="每日人品",
-    description="更加现代化的 NoneBot2 每日人品插件，支持查询今日、本周、本月和历史平均人品，提供详细的运势评价，并支持数据持久化存储。",
+    name="每日人品 3",
+    description="更加现代化的 NoneBot2 每日人品插件，支持查询今日、本周、本月和历史平均人品，自定义运势，以及数据持久化存储。",
     usage="jrrp/今日人品/今日运势 - 查询今日人品指数\nweekjrrp/本周人品/本周运势/周运势 - 查询本周平均人品\nmonthjrrp/本月人品/本月运势/月运势 - 查询本月平均人品\nalljrrp/总人品/平均人品/平均运势 - 查询历史平均人品",
 
     type="application",
